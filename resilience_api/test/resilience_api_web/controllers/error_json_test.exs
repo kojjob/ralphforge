749fefccfba9d78eed39768fc5542a8d8b61ac03
Kojo <kodeon6@gmail.com>
2026-01-10 20:55:01 +0000

    🔥 initial commit 🔥

---
+++ resilience_api/test/resilience_api_web/controllers/error_json_test.exs
@@ -0,0 +1,12 @@
+defmodule ResilienceApiWeb.ErrorJSONTest do
+  use ResilienceApiWeb.ConnCase, async: true
+
+  test "renders 404" do
+    assert ResilienceApiWeb.ErrorJSON.render("404.json", %{}) == %{errors: %{detail: "Not Found"}}
+  end
+
+  test "renders 500" do
+    assert ResilienceApiWeb.ErrorJSON.render("500.json", %{}) ==
+             %{errors: %{detail: "Internal Server Error"}}
+  end
+end
---
+++ resilience_api/test/test_helper.exs
@@ -0,0 +1,2 @@
+ExUnit.start()
+Ecto.Adapters.SQL.Sandbox.mode(ResilienceApi.Repo, :manual)
